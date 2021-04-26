c035dfece0452c77c29d1e93b3a939f528697fc4
SmolByte <nikhilparikh@hotmail.com>
2021-04-26 19:31:05 -0400

    error handling

--- app.js
+++ app.js
@@ -19,4 +19,11 @@ app.use((req, res, next) => {
     next(error);
 })
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: 'Ya messed up, bad. TRY AGAIN'
+    });
+});
+
 module.exports = app;
