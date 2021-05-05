8f93fea182e415d6602c38d001143b617c2258c7
SmolByte <nikhilparikh@hotmail.com>
2021-05-05 13:11:07 -0400

    connected routes to app.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,5 @@
 /**
- * The routes to access the differernt specific pokemon 
+ * The routes to access the different specific users
  * 
  * @authors: Nikhil Parikh, Xavier Williams 
  */
@@ -19,5 +19,50 @@ router.get('/:userID', async (req, res, next) => {
         });
     }
 });
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Handling POST requests to /user"
+    });
+
+});
+
+module.exports = (req, res, next) => {
+    if (!req.session.isLoggedIn && !req.session.userinfo) {
+        return res.redirect("/redirect/login.pug");
+    }
+    next();
+}
+  
+router.get("/", (req, res) => {
+    if (req.session && req.session.userinfo && req.session.isLoggedIn) {
+      return res.redirect("/signup.pug");
+    } else {
+      return res.redirect("/redirect/login.pug");
+    }
+  });
+
+  
+  router.get("/redirect/login.pug", authController.redirectlogin.pug);
+  router.get("/auth/login/callback", authController.callbacklogin.pug);
+  router.get("/redirect/logout", authController.logOut);
+  
+  
+  router.param('userID', function(req, res, next, value){
+    console.log('Request for a User ' + value);
+    next();
+})
+
+router.get('/userID', async function(req, res){
+   try {
+       let result = await _id.findOne({_id: ObjectID(req.params.userID)});
+       console.log(result);
+
+       res.render('name', {name: result.name})
+   } catch(e){
+       console.log(e.message);
+   }
+});
+
 
 module.exports = router;
